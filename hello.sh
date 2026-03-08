9eabe5fc1b186f0d6eabd7d81f448543228a25f1
Developerajay8374 <ajaymandaloji34@gmail.com>
2026-03-08 14:23:40 +0530

    empty

--- hello.sh
+++
@@ -1,3 +0,0 @@
-echo "Hello"
-
-This line is adding to test git fetch and merge command.
--- test1.sh
+++
@@ -1,3 +0,0 @@
-echo "This line is just now added"
-test1 file need to test the cherrypic command
-echo "adding and commiting second time"
